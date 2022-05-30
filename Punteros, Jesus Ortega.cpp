1758f7176caf7aea5ad7dddf30632f6899da9433
agetro2003 <98996101+agetro2003@users.noreply.github.com>
2022-05-30 16:32:25 -0400

    feat: add files

---
+++ Punteros, Jesus Ortega.cpp
@@ -0,0 +1,39 @@
+// Nombre: Jesús Ortega
+// C.I.: 29.891.702
+
+// Ejercicio de punteros: operadores new y delete.
+
+/* Este programa utiliza los operadores new y delete en la creacion
+ de arreglos dinamicos.
+ De forma que el usuario introducira la cantidad de memoria que se reserva 
+ para el arreglo */
+
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+int *array, cantidad_numeros;
+
+int main() {
+	
+	printf ("Escriba la cantidad de numeros que desea introducir: \n ");
+	scanf ("%d", &cantidad_numeros);
+	
+	array = new int[cantidad_numeros] ;// Creando arreglo dinamico
+	
+	printf ("Introduzca los valores del arreglo: \n");
+	for(int i=0;i<cantidad_numeros;i++)	{
+		printf ("\n array[%d] = ", i);
+		scanf ("%d", &array[i]);
+	}
+	
+	printf ("\n El arreglo se muestra a continuacion: \n");
+	for(int i=0; i<cantidad_numeros;i++)	{
+		printf ("\n array[%d] = %d ", i,array[i]);
+	}
+	
+	delete[] array;
+	
+	return 0;
+}
+ 
