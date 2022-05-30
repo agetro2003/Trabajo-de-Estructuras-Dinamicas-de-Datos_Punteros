64091a73d509e78677a0d9444e252bbeb857596c
Jesús Ortega <98996101+agetro2003@users.noreply.github.com>
2022-05-30 16:48:07 -0400

    feat: añadir comentarios

--- Punteros, Jesus Ortega.cpp
+++ Punteros, Jesus Ortega.cpp
@@ -1,4 +1,4 @@
-// Nombre: Jesús Ortega
+// Nombre: JesÃºs Ortega
 // C.I.: 29.891.702
 
 // Ejercicio de punteros: operadores new y delete.
@@ -16,24 +16,26 @@ int *array, cantidad_numeros;
 
 int main() {
 	
+	//Creacion del arreglo dinamico
 	printf ("Escriba la cantidad de numeros que desea introducir: \n ");
 	scanf ("%d", &cantidad_numeros);
+	array = new int[cantidad_numeros] ;
 	
-	array = new int[cantidad_numeros] ;// Creando arreglo dinamico
-	
+	//Ciclo para recibir valores
 	printf ("Introduzca los valores del arreglo: \n");
 	for(int i=0;i<cantidad_numeros;i++)	{
 		printf ("\n array[%d] = ", i);
 		scanf ("%d", &array[i]);
 	}
 	
+	//Ciclo para mostrar valores
 	printf ("\n El arreglo se muestra a continuacion: \n");
 	for(int i=0; i<cantidad_numeros;i++)	{
 		printf ("\n array[%d] = %d ", i,array[i]);
 	}
-	
+	//liberar memoria del arreglo
 	delete[] array;
-	
+	//Fun del codigo
 	return 0;
 }
  
